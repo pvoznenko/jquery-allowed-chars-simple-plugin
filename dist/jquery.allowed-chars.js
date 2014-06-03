3a04815670e93cbdfec376e8e623799e1369cc5f
Pavel Voznenko <p.voznenko@traviangames.com>
2014-06-03 11:46:52 +0200

    1.0.4 travis added

--- dist/jquery.allowed-chars.js
+++ dist/jquery.allowed-chars.js
@@ -1,5 +1,5 @@
 /**
- * jQuery Allowed Chars simple plugin version: 1.0.3, date: 27/05/2014
+ * jQuery Allowed Chars simple plugin version: 1.0.4, date: 27/05/2014
  *
  * jQuery plugin to restrict users for typing only allowed chars (regExp) for specified element
  *
